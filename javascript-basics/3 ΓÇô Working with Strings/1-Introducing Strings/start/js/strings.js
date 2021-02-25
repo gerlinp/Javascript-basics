a717ce778bdac9a1e2b7a7f7de4e70cb4009250f
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-25 01:00:56 -0500

    Strings

--- javascript-basics/3 ΓÇô Working with Strings/1-Introducing Strings/start/js/strings.js
+++ javascript-basics/3 ΓÇô Working with Strings/1-Introducing Strings/start/js/strings.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const htmlSnippet = '<h1 class="headline"> My Headline</h1>';
+const message = 'I\'m a programmer!';
+
+const multiline = "Hello, students. \
+Welcome to JavaScript Baics. \
+I hope you learn a lot!";
+
+console.log(multiline);
