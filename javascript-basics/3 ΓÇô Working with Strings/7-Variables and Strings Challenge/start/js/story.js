fc85c304497c0eb64a05f9ac3ae88ea7c2680f6e
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-25 17:41:45 -0500

    story challenge

--- javascript-basics/3 ΓÇô Working with Strings/7-Variables and Strings Challenge/start/js/story.js
+++ javascript-basics/3 ΓÇô Working with Strings/7-Variables and Strings Challenge/start/js/story.js
@@ -4,8 +4,9 @@ let ans1 = prompt("What is your name");
 
 // 2. Combine the input with other words to create a story.
 
-let qst1 = `Your name is ${ans1}. Did I get that right?`
+let qst1 = `<p>Your name is ${ans1}. Did I get that right?</p>`
 
 
 // 3. Display the story as a <p> inside the <main> element.
 
+document.querySelector('main').innerHTML = qst1;
