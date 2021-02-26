8af100ee9e254b5c9b8cec4c78a09898feb8e4f5
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-25 19:56:38 -0500

    Update conditionals.js

---
+++ javascript-basics/4 ΓÇô Making Decisions in Your Code with Conditional Statements/1-Introducing Conditional Statements/start/js/conditionals.js
@@ -0,0 +1,7 @@
+const answer = prompt('Which planet is closest to the sun?');
+
+if ( answer === 'Mercury' ) {
+    console.log("That's correct!");
+} else {
+    console.log("Sorry, that's incorrect");
+}
