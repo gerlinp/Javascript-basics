3004f3eae2aeabd1e85962fcc8d324ad55711595
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-25 20:22:37 -0500

    boolean values

--- javascript-basics/4 ΓÇô Making Decisions in Your Code with Conditional Statements/1-Introducing Conditional Statements/start/js/booleans.js
+++ javascript-basics/4 ΓÇô Making Decisions in Your Code with Conditional Statements/1-Introducing Conditional Statements/start/js/booleans.js
@@ -1,6 +1,14 @@
 
-if () {
-  console.log('The condition is true.');
+let correctGuess = false;
+const number = 6;
+const guess = prompt('Guess a number between 1 and 10.');
+
+if ( +guess === number) {
+  correctGuess = true;  
+}
+
+if ( correctGuess === true ) {
+  console.log('You guessed the number!');
 } else {
-  console.log('The condition is false.');
+  console.log(`Sorry, The number was ${number}`);
 }
